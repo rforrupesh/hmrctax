ea811c68fc25b94aac6ce7d3a22d686b1aaec67a
rforrupesh <rupeshkumar4775@gmail.com>
2026-09-21 21:06:15 +0530

    b

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://rforrupesh.github.io',
+  base: '/hmrctax',
   integrations: [sitemap()],
   build: {
     format: 'directory'
