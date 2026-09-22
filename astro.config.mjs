409713d1cd55efcc22729c8c05b469fc25cecaa9
rforrupesh <rupeshkumar4775@gmail.com>
2026-09-22 09:34:22 +0530

    shiv_03

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+//
+// Currently deployed at: https://rforrupesh.github.io/hmrctax/
+// When you add a custom domain later, change this to:
+//   site: 'https://hmrctax.co.uk',
+//   base: '/',   (or remove base entirely)
 export default defineConfig({
-  site: 'https://hmrctax.co.uk',
+  site: 'https://rforrupesh.github.io',
+  base: '/hmrctax/',
   integrations: [sitemap()],
 });
