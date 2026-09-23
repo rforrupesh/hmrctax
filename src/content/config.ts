2667192234afb56fa32df8ba6f8d6f36ffd1b0f7
rforrupesh <rupeshkumar4775@gmail.com>
2026-09-23 20:30:09 +0530

    v1

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,4 +15,15 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const pages = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    heroLead: z.string().optional(),
+    image: z.string().optional(),
+    takeaways: z.array(z.string()).default([]),
+  }),
+});
+
+export const collections = { blog, pages };
